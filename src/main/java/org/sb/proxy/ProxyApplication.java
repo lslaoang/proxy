185151b344361eba622cb72df2031ea2b824cbb2
Laurel Laoang, Jr <laurellaoang@gmail.com>
2024-05-24 15:27:09 +0200

    configure azure

--- src/main/java/org/sb/proxy/ProxyApplication.java
+++ src/main/java/org/sb/proxy/ProxyApplication.java
@@ -3,15 +3,20 @@ package org.sb.proxy;
 import com.azure.core.annotation.Get;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
 @RestController
 public class ProxyApplication {
 
-    @GetMapping("/proxy")
+    @GetMapping("/")
     public String hello() {
+        System.out.print(SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         return "Hello World";
     }
 
--- src/main/java/org/sb/proxy/configuration/WebSecurityConfig.java
+++ src/main/java/org/sb/proxy/configuration/WebSecurityConfig.java
@@ -1,4 +1,23 @@
 package org.sb.proxy.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class WebSecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .oauth2Login(Customizer.withDefaults())
+                .authorizeHttpRequests(
+                request -> request.requestMatchers("/").authenticated()
+                        .anyRequest().authenticated()
+
+        );
+        return http.build();
+    }
 }
